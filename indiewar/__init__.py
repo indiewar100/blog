82f661efa5a5dd71784bffe062e3684fe6ab7988
indiewar <indiewar100@gmail.com>
2018-12-18 21:22:53 +0800

    Update __init__.py

--- indiewar/__init__.py
+++ indiewar/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from bluelog.settings import config
+from indiewar.settings import config
 
 
 def create_app(config_name=None):
     if config_name is None:
         config_name = os.getenv('FLASK_CONFIG', 'development')
 
-    app = Flask('indiewarblog')
+    app = Flask('indiewar')
     app.config.from_object(config[config_name])
